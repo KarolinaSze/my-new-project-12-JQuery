cdb14a6ffe39a2bf598de769fedbffb7fd640371
Karolina <szefu6@wp.pl>
2017-03-15 19:31:48 +0100

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,8 @@
 // plik scripts.js
 
-var span = $("span"); 
-span.each(function(index, element) {
-});
-
+var span = $('span'); 
 span.each(function(index, element) { 
-	if(index % 2 == 0) { 
+	if (index % 2 == 0) { 
 		$(element).css('color', 'red'); 
 	}; 
 });
